82eeab44294ebb060b0abf1dd03063730ef9da83
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-07-15 16:30:57 +0200

    set up backend

---
+++ ShareMe/shareme_backend/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'ntymg9ah',
+    dataset: 'production'
+  }
+})
---
+++ ShareMe/shareme_backend/schemaTypes/index.js
@@ -0,0 +1,12 @@
+
+import user from './user';
+import pin from './pin';
+import postedBy from './postedBy';
+import comment from './comment';
+import save from './save';
+
+
+export const schemaTypes = [user, pin, comment, save, postedBy]
+
+
+
