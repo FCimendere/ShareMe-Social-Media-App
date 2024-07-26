ea0a919e1f2dae50d0708bc607b64cf6bc2152bf
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-07-26 15:53:47 +0200

    create all feed page

--- ShareMe/shareme_frontend/src/components/Feed.jsx
+++ ShareMe/shareme_frontend/src/components/Feed.jsx
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom'; // use for discovering the query categories
 
+import { client } from '../client'; // sanity client
+import MasonryLayout from './MasonryLayout'; // natural looked masonry layout fro different sizes grids
+import Spinner from './Spinner';
+import { feedQuery, searchQuery } from '../utils/data';
 
 const Feed = () => {
+  const [loading, setLoading] = useState(false);
+  const [pins, setPins] = useState(null);
+  
+  // collecting category info from query
+  const { categoryId } = useParams();
+
+
+  useEffect (() => {
+    setLoading(true);
+    
+    if(categoryId) {
+        const query = searchQuery(categoryId);
+
+        client.fetch(query)
+          .then((data) => {
+          setPins(data);
+          setLoading(false);
+          });
+    } else {
+      client.fetch(feedQuery)
+        .then((data)=> {
+          setPins(data);
+          setLoading(false);
+        })
+    }
+  }, [categoryId])
+
+  if(loading) return < Spinner message='We are adding new ideas to your feed!' />
+
   return (
     <div>
-      Feed
+      {pins && < MasonryLayout pins={pins} />} 
     </div>
   )
 }
