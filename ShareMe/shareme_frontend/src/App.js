bdc731105523b061a6fb6752f50e8bcd5b4adcb7
Fulya Cimendere <fulyacoskun.87@gmail.com>
2024-08-01 15:13:14 +0200

    update effect hook in app.js

--- ShareMe/shareme_frontend/src/App.js
+++ ShareMe/shareme_frontend/src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useEffect } from 'react';
 import {Routes, Route, useNavigate} from 'react-router-dom';
 
 
 import Login from "./components/Login";
 import Home from "./container/Home";
 
-function App() {
+
+
+const App = () =>  {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const storedUser = localStorage.getItem('user');
+        let user = null;
+        
+        if (storedUser && storedUser !== 'undefined') {
+            try {
+                user = JSON.parse(storedUser);
+            } catch (error) {
+                console.error('Error parsing stored user:', error);
+                localStorage.clear();
+            }
+        } else {
+            localStorage.clear();
+        }
+        if (!user) navigate('/login');
+    }, [navigate]);
+
     return(
     //    <h1 className="text-3xl font-bold underline">Hello World!</h1>
     <Routes>
